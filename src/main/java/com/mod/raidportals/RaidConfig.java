410c58b277437a469612f3ac586d505d9562fac9
anxious1 <SanekBallas@mail.ru>
2025-05-24 20:56:52 +0300

    заготовка конфига

--- src/main/java/com/mod/raidportals/RaidConfig.java
+++ src/main/java/com/mod/raidportals/RaidConfig.java
@@ -1,12 +1,76 @@
 package com.mod.raidportals;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import java.util.List;
 
 public class RaidConfig {
     public static final ForgeConfigSpec COMMON_SPEC;
+    public static final Common COMMON;
+
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        // тут будут параметры
+        COMMON = new Common(builder);
         COMMON_SPEC = builder.build();
+    }
+
+    /** Общий (COMMON) раздел конфига */
+    public static class Common {
+        // Списки ID боссов по уровням
+        public final ForgeConfigSpec.ConfigValue<List<? extends String>> bossesLevel1;
+        public final ForgeConfigSpec.ConfigValue<List<? extends String>> bossesLevel2;
+        public final ForgeConfigSpec.ConfigValue<List<? extends String>> bossesLevel3;
+
+        // Мин/макс монет
+        public final ForgeConfigSpec.IntValue minCoins;
+        public final ForgeConfigSpec.IntValue maxCoins;
+
+        // Макс HP боссов по уровням
+        public final ForgeConfigSpec.IntValue maxHpLevel1;
+        public final ForgeConfigSpec.IntValue maxHpLevel2;
+        public final ForgeConfigSpec.IntValue maxHpLevel3;
+
+        public Common(ForgeConfigSpec.Builder builder) {
+            builder.push("raid_portals");
+
+            // ID боссов
+            bossesLevel1 = builder
+                    .comment("Список ID боссов для уровня сложности 1")
+                    .defineList("bossesLevel1",
+                            List.of("modid:boss1", "modid:boss2"),
+                            o -> o instanceof String
+                    );
+
+            bossesLevel2 = builder
+                    .comment("Список ID боссов для уровня сложности 2")
+                    .defineList("bossesLevel2",
+                            List.of("modid:boss3", "modid:boss4"),
+                            o -> o instanceof String
+                    );
+
+            bossesLevel3 = builder
+                    .comment("Список ID боссов для уровня сложности 3")
+                    .defineList("bossesLevel3",
+                            List.of("modid:boss5", "modid:boss6"),
+                            o -> o instanceof String
+                    );
+
+            // Мин/макс монет
+            builder.comment("Минимальное и максимальное количество монет рейда");
+            minCoins = builder
+                    .defineInRange("minCoins",  5, 0, Integer.MAX_VALUE);
+            maxCoins = builder
+                    .defineInRange("maxCoins", 10, 0, Integer.MAX_VALUE);
+
+            // Настройка HP боссов
+            builder.comment("Максимальное HP боссов для каждого уровня");
+            maxHpLevel1 = builder
+                    .defineInRange("maxHpLevel1", 3000, 1, Integer.MAX_VALUE);
+            maxHpLevel2 = builder
+                    .defineInRange("maxHpLevel2", 5000, 1, Integer.MAX_VALUE);
+            maxHpLevel3 = builder
+                    .defineInRange("maxHpLevel3", 8000, 1, Integer.MAX_VALUE);
+
+            builder.pop();
+        }
     }
 }
--- src/main/java/com/mod/raidportals/RaidPortalsMod.java
+++ src/main/java/com/mod/raidportals/RaidPortalsMod.java
@@ -1,10 +1,17 @@
 package com.mod.raidportals;
 
+import com.mod.raidportals.ModRegistry;
+import com.mod.raidportals.RaidConfig;
+import com.mod.raidportals.RaidEventHandlers;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
@@ -14,16 +21,47 @@ public class RaidPortalsMod {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public RaidPortalsMod() {
-        // Инициализация регистрации блоков и предметов
+        // 1) Регистрация блоков и предметов
         ModRegistry.init(FMLJavaModLoadingContext.get().getModEventBus());
 
-        // Регистрация общего конфига
+        // 2) Регистрация COMMON-конфига
         ModLoadingContext.get()
                 .registerConfig(ModConfig.Type.COMMON, RaidConfig.COMMON_SPEC);
 
-        // Подписка на шину игровых событий (для RaidEventHandlers)
+        // 3) Подписка на игровые события
         MinecraftForge.EVENT_BUS.register(RaidEventHandlers.class);
 
+        // 4) Клиентская и общая загрузка
+        FMLJavaModLoadingContext.get()
+                .getModEventBus()
+                .addListener(this::onClientSetup);
+        FMLJavaModLoadingContext.get()
+                .getModEventBus()
+                .addListener(this::onCommonSetup);
+
         LOGGER.info("RaidPortalsMod initialized");
+    }
+
+    /** Клиентские настройки (если нужны рендер-слои и т.п.) */
+    private void onClientSetup(final FMLClientSetupEvent event) {
+        // здесь можно добавить RenderTypeLookup либо оставить пустым,
+        // если модели уже содержат render_type в JSON
+    }
+
+    /** Общие настройки: логируем значения из конфига при старте */
+    private void onCommonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("=== RaidPortals CONFIG ===");
+        LOGGER.info("Level1 bosses = {}", RaidConfig.COMMON.bossesLevel1.get());
+        LOGGER.info("Level2 bosses = {}", RaidConfig.COMMON.bossesLevel2.get());
+        LOGGER.info("Level3 bosses = {}", RaidConfig.COMMON.bossesLevel3.get());
+        LOGGER.info("Coins range = {}..{}",
+                RaidConfig.COMMON.minCoins.get(),
+                RaidConfig.COMMON.maxCoins.get()
+        );
+        LOGGER.info("Max HP = L1:{} L2:{} L3:{}",
+                RaidConfig.COMMON.maxHpLevel1.get(),
+                RaidConfig.COMMON.maxHpLevel2.get(),
+                RaidConfig.COMMON.maxHpLevel3.get()
+        );
     }
 }
